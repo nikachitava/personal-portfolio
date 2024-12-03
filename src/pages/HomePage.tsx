e085b0ae8cccd096d2d50458533f188f0ae974e5
nikachitava <chitava18.official@gmail.com>
2024-12-03 22:02:22 +0400

    create hero section

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,11 @@
+import HeroSection from "../sections/HeroSection";
+
 const HomePage = () => {
-	return <div>HomePage</div>;
+	return (
+		<>
+			<HeroSection />
+		</>
+	);
 };
 
 export default HomePage;
--- src/sections/HeroSection.tsx
+++ src/sections/HeroSection.tsx
@@ -1,5 +1,21 @@
+import "animate.css";
+
 const HeroSection = () => {
-	return <div>HeroSection</div>;
+	return (
+		<section className="flex items-center justify-between animate__animated animate__slideInDown">
+			<h1 className="font-bold font-Poppins text-heading dark:text-[#D9D9D9] text-6xl leading-[70px] animate__animated animate__zoomIn">
+				Hi 👋, <br />
+				My name is <br />
+				<span className="text-gradient">nika chitava</span>
+				<br /> I build things for web
+			</h1>
+			<img
+				src="/images/nk1.png"
+				alt="image"
+				className="animate__animated animate__zoomIn"
+			/>
+		</section>
+	);
 };
 
 export default HeroSection;
