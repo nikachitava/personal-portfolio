36f9d9a7144b944a5d47005814256e4fe031cddf
nikachitava <chitava18.official@gmail.com>
2024-12-04 20:26:32 +0400

    change image hero section

--- src/sections/HeroSection.tsx
+++ src/sections/HeroSection.tsx
@@ -2,17 +2,18 @@ import "animate.css";
 
 const HeroSection = () => {
 	return (
-		<section className="flex items-center justify-center lg:justify-between">
+		<section className="flex items-center justify-center lg:justify-between min-h-[50vh]">
 			<h1 className="font-bold font-Poppins text-heading dark:text-[#D9D9D9] text-6xl leading-[70px]">
 				Hi 👋, <br />
 				My name is <br />
 				<span className="text-gradient">nika chitava</span>
-				<br /> I build things for web
+				<br /> I'm Front-end Developer
 			</h1>
 			<img
-				src="/images/nk1.png"
+				src="/images/nc_icon.jpg"
 				alt="image"
-				className="hidden lg:block"
+				loading="lazy"
+				className="hidden lg:block rounded-full w-1/4 h-auto"
 			/>
 		</section>
 	);
