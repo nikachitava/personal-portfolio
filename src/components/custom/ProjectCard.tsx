a36aa95d771a15b8196fc7dc0fdf2cd6b74818ec
nikachitava <chitava18.official@gmail.com>
2024-12-13 19:26:12 +0400

    change some details on projectcard

--- src/components/custom/ProjectCard.tsx
+++ src/components/custom/ProjectCard.tsx
@@ -29,64 +29,58 @@ const ProjectCard: React.FC<IProjectCardProps> = ({
 	};
 
 	return (
-		<Link to={`/project/${_id}`}>
-			<div
-				key={_id}
-				className="w-[375px] rounded-[20px] bg-white dark:bg-[#363636] overflow-hidden shadow-custom-blur relative"
-			>
-				{isAuthenticated && _id && (
-					<div
-						className="absolute right-[10px] top-[10px] bg-slate-500 p-2 text-center rounded-full hover:bg-slate-700 cursor-pointer"
-						onClick={() => deleteProject(_id)}
-					>
-						<MdDeleteSweep size={30} color="white" />
-					</div>
-				)}
-				<img
-					src={image[0]}
-					alt="cover"
-					className="object-fill w-[375px] h-[200px]"
-				/>
-				<div className="p-7 space-y-3">
-					<h1 className="font-Poppins text-2xl font-medium text-black dark:text-solidHeadingDarkMode">
+		<div
+			key={_id}
+			className="w-[375px] rounded-[20px] bg-white dark:bg-[#363636] overflow-hidden shadow-custom-blur relative"
+		>
+			{isAuthenticated && _id && (
+				<div
+					className="absolute right-[10px] top-[10px] bg-slate-500 p-2 text-center rounded-full hover:bg-slate-700 cursor-pointer"
+					onClick={() => deleteProject(_id)}
+				>
+					<MdDeleteSweep size={30} color="white" />
+				</div>
+			)}
+			<img
+				src={image[0]}
+				alt="cover"
+				className="object-fill w-[375px] h-[200px]"
+			/>
+			<div className="p-7 space-y-5">
+				<Link to={`/project/${_id}`}>
+					<h1 className="font-Poppins text-2xl mb-3 font-medium text-black dark:text-solidHeadingDarkMode">
 						{name}
 					</h1>
-					<p className="font-Poppins text-lg text-darkContent dark:text-solidHeadingDarkMode break-words">
+					<p className="font-Poppins text-lg mb-3 text-darkContent dark:text-solidHeadingDarkMode break-words">
 						{description.slice(0, 150)} ....{" "}
 						<span className="font-bold underline cursor-pointer">
 							READ MODE
 						</span>
 					</p>
-					<p className="font-Poppins text-heading dark:text-solidHeadingDarkMode">
-						Tech Stack:{" "}
-						<span className="text-sm font-light text-heading dark:text-solidHeadingDarkMode">
-							{tech_stack}
-						</span>
+					<p className="font-Poppins underline underline-offset-2  text-heading dark:text-solidHeadingDarkMode">
+						Click to get more information
 					</p>
-					<div className="flex items-center justify-between">
-						<div className="flex items-center gap-3">
-							<img
-								src="/images/icons/linkicon.svg"
-								alt="link_icon"
-							/>
-							<Link to={live_link}>
-								<p className="font-Poppins text-black underline dark:text-white">
-									Live Preview
-								</p>
-							</Link>
-						</div>
-						<div className="flex items-center gap-3">
-							<img src="/images/icons/git2.svg" alt="link_icon" />
-							<Link to={github_link}>
-								<p className="font-Poppins text-black underline dark:text-white">
-									Github Code
-								</p>
-							</Link>
-						</div>
+				</Link>
+				<div className="flex items-center justify-between">
+					<div className="flex items-center gap-3">
+						<img src="/images/icons/linkicon.svg" alt="link_icon" />
+						<Link to={live_link}>
+							<p className="font-Poppins text-black underline dark:text-white">
+								Live Preview
+							</p>
+						</Link>
+					</div>
+					<div className="flex items-center gap-3">
+						<img src="/images/icons/git2.svg" alt="link_icon" />
+						<Link to={github_link}>
+							<p className="font-Poppins text-black underline dark:text-white">
+								Github Code
+							</p>
+						</Link>
 					</div>
 				</div>
 			</div>
-		</Link>
+		</div>
 	);
 };
 
